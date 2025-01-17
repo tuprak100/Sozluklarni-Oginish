3c2e5c872350379ae2ecf17eed4eed6ef6d9f697
tuprak100 <57823962+tuprak100@users.noreply.github.com>
2025-01-17 09:35:36 -0600

    Update script.js
    changed front and back image locations.

--- script.js
+++ script.js
@@ -5,20 +5,16 @@ const nextCardButton = document.getElementById('next-card');
 const flipCardButton = document.getElementById('flip-card');
 const music = document.getElementById('background-music');
 let currentCardIndex = 0;
+
 const cards = [
-  { front: "https://raw.githubusercontent.com/tuprak100/Sozluklarni-Oginish/main/images/front_1.png", back: "https://raw.githubusercontent.com/tuprak100/Sozluklarni-Oginish/main/images/back_1.png" },
-  { front: "https://raw.githubusercontent.com/tuprak100/Sozluklarni-Oginish/main/images/front_2.png", back: "https://raw.githubusercontent.com/tuprak100/Sozluklarni-Oginish/main/images/back_2.png" },
+  { front: "https://raw.githubusercontent.com/tuprak100/Sozluklarni-Oginish/main/images/fronts/front_1.png", back: "https://raw.githubusercontent.com/tuprak100/Sozluklarni-Oginish/main/images/backs/back_1.png" },
+  { front: "https://raw.githubusercontent.com/tuprak100/Sozluklarni-Oginish/main/images/fronts/front_2.png", back: "https://raw.githubusercontent.com/tuprak100/Sozluklarni-Oginish/main/images/backs/back_2.png" },
   // ... more cards
 ];
 
 function loadCard(index) {
   front.innerHTML = `<img src="${cards[index].front}" alt="Front">`;
-  fetch(cards[index].back)
-    .then(response => response.blob()) // Or response.blob() for images; response.text for texts.
-    .then(data => {
-      const imageUrl = URL.createObjectURL(data); // Create a temporary URL for the image blob
-      back.innerHTML = `<img src="${imageUrl}" alt="Back">`;
-    });
+  back.innerHTML = `<img src="${cards[index].back}" alt="Back">`;
 }
 
 nextCardButton.addEventListener('click', () => {
